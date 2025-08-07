375179c2e48b6f205138a1002f20bc663f9425e3
lambdv <scafiy17@gmail.com>
2025-08-07 17:51:19 +1200

    added tailwind css to welcome page via partial header component

--- storage/framework/views/cc9db3b0fedaf3a993925aa7256b1681.php
+++ storage/framework/views/cc9db3b0fedaf3a993925aa7256b1681.php
@@ -1,11 +1,13 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>{{ $title ?? config('app.name') }}</title>
+<title><?php echo e($title ?? config('app.name')); ?></title>
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-@vite(['resources/css/app.css', 'resources/js/app.js'])
-@fluxAppearance
+<?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+<?php echo app('flux')->fluxAppearance(); ?>
+
+<?php /**PATH C:\Users\Lokesh\Herd\titanbox\resources\views/partials/head.blade.php ENDPATH**/ ?>
