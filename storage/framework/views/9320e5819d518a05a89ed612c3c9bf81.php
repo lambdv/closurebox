812c9edd330dfbf1d6234d62f2ce5f8b1d11f8c3
lambdv <scafiy17@gmail.com>
2025-08-07 16:30:10 +1200

    fixed inertia

---
+++ storage/framework/views/9320e5819d518a05a89ed612c3c9bf81.php
@@ -0,0 +1,6 @@
+
+<ui-close data-flux-modal-close>
+    <?php echo e($slot); ?>
+
+</ui-close>
+<?php /**PATH C:\Users\Lokesh\Herd\titanbox\vendor\livewire\flux\src/../stubs/resources/views/flux/modal/close.blade.php ENDPATH**/ ?>
